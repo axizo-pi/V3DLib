1df89da94172f76998579b50f2f4f944436e5f14
Wim Rijnders <wrijnders@gmail.com>
2025-11-05 20:16:43 +0100

    Further expansion of Instruction app.

--- Examples/Instruction.cpp
+++ Examples/Instruction.cpp
@@ -1,10 +1,61 @@
+/**
+ * NOTES
+ * =====
+ *
+ * ALU
+ * ===
+ * - waddr and a/b raddr for both add and mul 6 bits
+ * - add raddr's: smallest value always put in a from instr struct
+ * - mul raddr's: order preserved
+ * - sig_addr does not register, maybe dependent on sig
+ * - various sig's can not be combined; not all combinations are valid
+ *   - following verified ok: thrsw ldunif
+ * - raddr_a, raddr_b do not register on 7.x
+ * - mul add output_pack can not be disasm'd for nonzero values
+ * - fadd, faddnf: same packed
+ *
+ *
+ * BRANCH
+ * ======
+ *
+ * - Adresses in instr get rounded down to 4-bit boundaries on pack.
+ *   This makes sort of sense since instructions are 64 bits.
+ */
 #include "v3d/instr/v3d_api.h"
 #include "broadcom/qpu/qpu_disasm.h"
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 
 using namespace std;
+
+namespace {
+
+string line = 
+"_________________________________________________________________________________________________\n";
+
+string h =
+"|63|62|61|60|59|58|57|56|55|54|53|52|51|50|49|48|47|46|45|44|43|42|41|40|39|38|37|36|35|34|33|32|\n"
+"|31|30|29|28|27|26|25|24|23|22|21|20|19|18|17|16|15|14|13|12|11|10|09|08|07|06|05|04|03|02|01|00|\n"
+	;
+
+std::string format_bits(uint64_t val) {
+	stringstream buf;
+
+	for (int n = 63; n >= 0; n--) {
+		int bit = (val >> n) & 1;
+
+		buf << "|" << setw(2) << bit;
+
+		if (n == 32) buf << "|\n";
+	}
+	buf << "|\n";
+
+	return buf.str();
+}
+
+} // anon namespace
 
 
 std::string b(bool val) {
@@ -108,7 +159,7 @@ void display(struct v3d_qpu_instr const &instr) {
 "  sig      :"  << disp_sig(instr.sig)              << ";\n"
 "  sig_addr : " << ((unsigned) instr.sig_addr)      << ";\n"
 "  sig_magic: " << b(instr.sig_magic)               << ";\n"  /* If the signal writes to a magic address */
-"  raddr_a  : " << ((unsigned) instr.raddr_a)       << ";\n"                      /* V3D 4.x */
+"  raddr_a  : " << ((unsigned) instr.raddr_a)       << "; (4.x)\n"                      /* V3D 4.x */
 "  raddr_b  : " << ((unsigned) instr.raddr_b)       << ";\n"                      /* V3D 4.x (holds packed small immediate in 7.x too) */
 "  flags    : " << disp_flags(instr.flags)          << ";\n"
 "\n";
@@ -122,15 +173,106 @@ void display(struct v3d_qpu_instr const &instr) {
 	cout << "};\n" ;
 }
 
+
+std::string instr_format_alu(uint64_t val) {
+
+	string h2 =
+"|  |  |  |  |  | 1| 0|  |  |  |  |  |  |  |  |  | apf?|mw|aw| mul waddr       | add waddr       |\n"
+"|  |  | a_pa|  |1?|  |1?| mul a raddr     | mul b raddr     | add a raddr     | add b raddr     |\n"
+	;
+
+	string desc = "\n"
+		"- mw  : mul magic write\n"
+		"- aw  : add magic write\n"
+		"- a_pa: add output pack\n"
+		"- ac: flags\n"
+		"  - 51: condition enabled\n"
+		"  - 50: 0\n"
+		"  - 49: negate condition\n"
+		"  - 48: 0=a, 1=b\n"
+		"- apf?: there is overlap with mpf. I don't get it yet\n"
+		"  - It looks like apf/mpf share the same field, and bit 50 is set for mul\n"
+		"    I get the impression that add/mul exclude each other on push\n"
+	;
+
+	stringstream buf;
+
+	buf << line << h << line << h2 << line << format_bits(val);
+	buf << line << desc;
+
+	return buf.str();
+}
+
+
+std::string instr_format_branch(uint64_t val) {
+
+	string h2 =
+"|  |  |  |  |  | 0| 1|  | offset_bottom                                                | cond   |\n"
+"| offset_top            |  | fign|  |  |  |  | bdu |ub| bdi |  |  |  |  |  |  |  |  |  |  |  |  |\n"
+	;
+
+	string desc = "\n"
+		"- offset_top   : top 8 bits of 32-bit offset address\n"
+		"- offset_bottom: lower 24 bits of 32-bit offset address. The bottom 3 bits are not counted\n"
+		"- fign         : msfign\n"
+		"- cond         : enum values of cond are not consecutive in the packed instr\n"
+
+	;
+
+	stringstream buf;
+	buf << line << h << line << h2 << line << format_bits(val);
+	buf << line << desc;
+
+	return buf.str();
+}
+
+
 int main(int argc, const char *argv[]) {
-	struct v3d_qpu_instr instr = {
+	struct v3d_qpu_instr instr_alu = {
 		.type = V3D_QPU_INSTR_TYPE_ALU,
-		.sig  = { 0 /* .thrsw = 1 */ },
-		.raddr_a = 0,
-		0
+		.sig  = {
+			//.thrsw = 1,
+			//.ldunif = 1,
+			//.ldunifa = 1
+			//.small_imm_d = 1
+	 	},
+		//.sig_addr = 64,
+		.flags = {
+		  //.ac = V3D_QPU_COND_IFA,
+		  //.mc = V3D_QPU_COND_IFA
+			//.apf = V3D_QPU_PF_PUSHZ,
+			//.mpf = V3D_QPU_PF_PUSHC,
+		},
+		.alu = {
+			.add = {
+				  //.op = V3D_QPU_A_FADDNF,
+			  	.a = { .raddr = 0 },
+			  	.b = { .raddr = 0 },
+		 	    .waddr = 0,
+					//.magic_write = true,
+			},
+			.mul = {
+				  //.op = V3D_QPU_M_FMOV,
+					//.output_pack = V3D_QPU_PACK_NONE  // Other values fail disasm
+			},
+		},
 	};
 
-	display(instr);
+	struct v3d_qpu_instr instr_branch = {
+		.type = V3D_QPU_INSTR_TYPE_BRANCH,
+		.branch = {
+			//.cond = V3D_QPU_BRANCH_COND_ANYNA,
+			//.msfign = V3D_QPU_MSFIGN_Q,
+      //.bdu = V3D_QPU_BRANCH_DEST_REL,
+			//.ub = true,
+			//.raddr_a = 63
+			.offset = 0 //0x00800018
+		}
+	};
+
+	auto &instr = instr_branch;
+
+	//display(instr);
 
 	uint64_t packed =  instr_pack(&instr);
 
@@ -138,6 +280,13 @@ int main(int argc, const char *argv[]) {
 	cout << "Packed: " << hex << packed << "\n";
 	cout << "Decode: " << qpu_decode(&instr) << "\n";
 	cout << "Disasm: " << qpu_disasm(packed) << "\n";
+
+	if (instr.type == V3D_QPU_INSTR_TYPE_ALU) {
+		cout << instr_format_alu(packed);
+	} else {
+		cout << instr_format_branch(packed);
+	}
+
 
   return 0;
 }
