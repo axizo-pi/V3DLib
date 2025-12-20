91ea81e11a3a044e52c40c1ea1c165c6c5138cdd
Wim Rijnders <wrijnders@gmail.com>
2025-12-20 11:11:11 +0100

    Further refactoring of vc4 Instr

--- Lib/vc4/Instr.cpp
+++ Lib/vc4/Instr.cpp
@@ -1,5 +1,6 @@
 #include "Instr.h"
-#include <cassert>
+#include "Support/basics.h"  // fatal()
+//#include <cassert>
 
 namespace V3DLib {
 namespace vc4 {
@@ -15,7 +16,7 @@ void Instr::tag(Tag in_tag, bool imm) {
   switch(m_tag) {
     case NOP:  // default
     case LI:
-      break;  // as is
+      break;   // as is
 
     case ROT:
       m_sig = 13;
@@ -30,18 +31,18 @@ void Instr::tag(Tag in_tag, bool imm) {
       break;
 
     case END:
-      m_sig = 3;
-      raddrb = 39;
+      m_sig  = 3;
+      raddrb = NOP_R;
       break;
 
     case LDTMU:
-      m_sig = 10;
-      raddrb = 39;
+      m_sig  = 10;
+      raddrb = NOP_R;
       break;
 
     case SINC:
     case SDEC:
-      m_sig = 0xe;
+      m_sig      = 0xe;
       m_sem_flag = 8;
       break;
   };
@@ -93,6 +94,176 @@ uint32_t Instr::low() const {
 
   assert(false);
   return 0;
+}
+
+
+uint64_t Instr::encode() const {
+ return (((uint64_t) high()) << 32) + low();
+}
+
+
+/**
+ * @brief Determine the regfile and index combination to use for writes, for the passed 
+ *        register definition 'reg'.
+ *
+ * This function deals exclusively with write values of the regfile registers.
+ *
+ * See also NOTES in header comment for `encodeSrcReg()`.
+ *
+ * @param reg   register definition for which to determine output value
+ * @param file  out-parameter; the regfile to use (either REG_A or REG_B)
+ *
+ * @return index into regfile (A, B or both) of the passed register
+ *
+ * ----------------------------------------------------------------------------------------------
+ * ## NOTES
+ *
+ * * The regfile location for `ACC4` is called `TMP_NOSWAP` in the doc. This is because
+ *   special register `r4` (== ACC4) is read-only.
+ *   TODO: check code if ACC4 is actually used. Better to name it TMP_NOSWAP
+ *
+ * * ACC5 has parentheses with extra function descriptions.
+ *   This implies that the handling of ACC5 differs from the others (at least, for ACC[0123])
+ *   TODO: Check code for this; is there special handling of ACC5? 
+ */
+uint32_t Instr::encodeDestReg(Reg reg, RegTag* file) {
+  // Selection of regfile for the cases where using A or B doesn't matter
+  RegTag AorB = REG_A;  // Select A as default
+  if (reg.tag == REG_A || reg.tag == REG_B) {
+    AorB = reg.tag;     // If the regfile is preselected in `reg`, use that.
+  }
+
+  switch (reg.tag) {
+    case REG_A:
+      assert(reg.regId >= 0 && reg.regId < NUM_RF);
+      *file = REG_A;
+      return reg.regId;
+
+    case REG_B:
+      assert(reg.regId >= 0 && reg.regId < NUM_RF);
+      *file = REG_B;
+      return reg.regId;
+
+    case ACC:
+      // See NOTES in header comment
+      assert(reg.regId >= 0 && reg.regId <= 5); // !! ACC4 is TMP_NOSWAP, *not* r4
+      *file = reg.regId == 5 ? REG_B : AorB; 
+      return NUM_RF + reg.regId;
+
+    case SPECIAL:
+      switch (reg.regId) {
+        case SPECIAL_RD_SETUP:      *file = REG_A; return VPMVCD_RD_SETUP;
+        case SPECIAL_WR_SETUP:      *file = REG_B; return VPMVCD_WR_SETUP;
+        case SPECIAL_DMA_LD_ADDR:   *file = REG_A; return VPM_LD_ADDR;
+        case SPECIAL_DMA_ST_ADDR:   *file = REG_B; return VPM_ST_ADDR;
+        case SPECIAL_VPM_WRITE:     *file = AorB;  return VPM_WRITE;
+        case SPECIAL_HOST_INT:      *file = AorB;  return HOST_INT;
+        case SPECIAL_TMUA:          *file = AorB;  return TMU0_S;
+        case SPECIAL_SFU_RECIP:     *file = AorB;  return SFU_RECIP;
+        case SPECIAL_SFU_RECIPSQRT: *file = AorB;  return SFU_RECIPSQRT;
+        case SPECIAL_SFU_EXP:       *file = AorB;  return SFU_EXP;
+        case SPECIAL_SFU_LOG:       *file = AorB;  return SFU_LOG;
+        default:                    break;
+      }
+
+    case NONE:
+      // NONE maps to 'NOP' in regfile.
+      *file = AorB;
+      return NOP_R;
+
+    default:
+      fatal("V3DLib: missing case in encodeDestReg");
+      return 0;
+  }
+}
+
+
+/**
+ * @brief Determine regfile index and the read field encoding for alu-instructions, for the passed 
+ *        register 'reg'.
+ *
+ * The read field encoding, output parameter `mux` is a bitfield in instructions `alu` and 
+ * 'alu small imm'. It specifies the register(s) to use as input.
+ *
+ * This function deals exclusively with 'read' values.
+ *
+ * @param reg   register definition for which to determine output value
+ * @param file  regfile to use. This is used mainly to validate the `regid` field in param `reg`. In specific
+ *              cases where both regfile A and B are valid (e.g. NONE), it is used to select the regfile.
+ * @param mux   out-parameter; value in ALU instruction encoding for fields `add_a`, `add_b`, `mul_a` and `mul_b`.
+ *
+ * @return index into regfile (A, B or both) of the passed register.
+ *
+ * ----------------------------------------------------------------------------------------------
+ * ## NOTES
+ *
+ * * There are four combinations of access to regfiles:
+ *   - read A
+ *   - read B
+ *   - write A
+ *   - write B
+ *
+ * This is significant, because SPECIAL registers may only be accessible through a specific combination
+ * of A/B and read/write.
+ *
+ * * References in VideoCore IV Reference document:
+ *
+ *   - Fields `add_a`, `add_b`, `mul_a` and `mul_b`: "Figure 4: ALU Instruction Encoding", page 26
+ *   - mux value: "Table 3: ALU Input Mux Encoding", page 28
+ *   - Index regfile: "Table 14: 'QPU Register Addess Map'", page 37.
+ *
+ * ----------------------------------------------------------------------------------------------
+ * ## TODO
+ *
+ * * NONE/NOP - There are four distinct versions for `NOP`, A/B and no read/no write.
+ *              Verify if those distinctions are important at least for A/B.
+ *              They might be the same thing.
+ */
+uint32_t Instr::encodeSrcReg(Reg reg, RegTag file, uint32_t* mux) {
+  assert (file == REG_A || file == REG_B);
+
+  const uint32_t NO_REGFILE_INDEX = 0;  // Return value to use when there is no regfile mapping for the register
+
+  // Selection of regfile for the cases that both A and B are possible.
+  // Note that param `file` has precedence here.
+  uint32_t AorB = (file == REG_A)? MUX_A: MUX_B;
+
+  switch (reg.tag) {
+    case REG_A:
+      assert(reg.regId >= 0 && reg.regId < NOP_R && file == REG_A);
+      *mux = 6;
+      return reg.regId;
+
+    case REG_B:
+      assert(reg.regId >= 0 && reg.regId < NOP_R && file == REG_B);
+      *mux = 7;
+      return reg.regId;
+
+    case ACC:
+      // ACC does not map onto a regfile for 'read'
+      assert(reg.regId >= 0 && reg.regId <= 4);  // TODO index 5 missing, is this correct??
+      *mux = reg.regId;
+      return NO_REGFILE_INDEX;
+
+    case NONE:
+      // NONE maps to `NOP` in the regfile
+      *mux = AorB;
+      return NOP_R;
+
+    case SPECIAL:
+      switch (reg.regId) {
+        case SPECIAL_UNIFORM:     *mux = AorB;                         return UNIFORM_READ;
+        case SPECIAL_ELEM_NUM:    assert(file == REG_A); *mux = MUX_A; return ELEMENT_NUMBER;
+        case SPECIAL_QPU_NUM:     assert(file == REG_B); *mux = MUX_B; return QPU_NUMBER;
+        case SPECIAL_VPM_READ:    *mux = AorB;                         return VPM_READ;
+        case SPECIAL_DMA_LD_WAIT: assert(file == REG_A); *mux = MUX_A; return VPM_LD_WAIT;
+        case SPECIAL_DMA_ST_WAIT: assert(file == REG_B); *mux = MUX_B; return VPM_ST_WAIT;
+      }
+
+    default:
+      fatal("V3DLib: missing case in encodeSrcReg");
+      return 0;
+  }
 }
 
 }  // namespace vc4
--- Lib/vc4/Instr.h
+++ Lib/vc4/Instr.h
@@ -1,6 +1,7 @@
 #ifndef _V3DLIB_VC4_INSTR_H_
 #define _V3DLIB_VC4_INSTR_H_
 #include <stdint.h>
+#include "Target/instr/Reg.h"
 
 namespace V3DLib {
 namespace vc4 {
@@ -19,19 +20,44 @@ public:
     LDTMU  // Always writes to ACC4
   };
 
-  uint32_t cond_add = 0;    // reused for LI, BR
+  enum Registers {
+    MUX_A           = 6,
+    MUX_B           = 7,
+
+    NUM_RF          = 32,
+    UNIFORM_READ    = 32,      // rfA/B rd
+    ELEMENT_NUMBER  = 38,      // rfA rd
+    QPU_NUMBER      = 38,      // rfB rd
+    HOST_INT        = 38,      // rfA/B wr
+    NOP_R           = 39,      // NOP for register read/writes
+    VPM_READ        = 48,      // rfA/B rd
+    VPM_WRITE       = 48,      // rfA/B wr
+    VPMVCD_RD_SETUP = 49,      // rfA wr
+    VPMVCD_WR_SETUP = 49,      // rfB wr
+    VPM_LD_WAIT     = 50,      // rfA rd
+    VPM_ST_WAIT     = 50,      // rfB rd
+    VPM_LD_ADDR     = 50,      // rfA wr
+    VPM_ST_ADDR     = 50,      // rfB wr
+    SFU_RECIP       = 52,      // rfA/B wr
+    SFU_RECIPSQRT   = 53,      // "
+    SFU_EXP         = 54,      // "
+    SFU_LOG         = 55,      // "
+    TMU0_S          = 56,      // "
+  };
+
+  uint32_t cond_add = 0;       // reused for LI, BR
   uint32_t cond_mul = 0;
-  uint32_t waddr_add = 39;  // reused for LI
-  uint32_t waddr_mul = 39;
+  uint32_t waddr_add = NOP_R;  // reused for LI
+  uint32_t waddr_mul = NOP_R;
 
   uint32_t addOp  = 0;
   uint32_t mulOp  = 0;
   uint32_t muxa   = 0;
   uint32_t muxb   = 0;
-  uint32_t raddra = 39;
+  uint32_t raddra = NOP_R;
   uint32_t raddrb = 0;
 
-  uint32_t li_imm = 0;  // Also used as BR target
+  uint32_t li_imm  = 0;         // Also used as BR target
   uint32_t sema_id = 0;
 
   void sf(bool val);
@@ -39,14 +65,14 @@ public:
   void rel(bool val);
 
   void tag(Tag in_tag, bool imm = false);
+  uint64_t encode() const;
 
-  uint64_t encode() const {
-   return (((uint64_t) high()) << 32) + low();
-  }
+  static uint32_t encodeDestReg(Reg reg, RegTag* file);
+  static uint32_t encodeSrcReg(Reg reg, RegTag file, uint32_t* mux);
 
 private:
-  Tag m_tag = NOP;
-  uint32_t m_sig = 14;        // 0xe0000000
+  Tag m_tag           = NOP;
+  uint32_t m_sig      = 14;   // 0xe0000000
   uint32_t m_sem_flag = 0;    // TODO research what this is for, only used with SINC/SDEC
 
   bool m_ws  = false;
