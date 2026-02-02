1808bc8fc799d447fc73f60918309404d0aa90bc
Wim Rijnders <wrijnders@gmail.com>
2026-02-02 08:15:57 +0100

    Rot3D edits during profiling

--- Lib/Kernels/Rot3D.cpp
+++ Lib/Kernels/Rot3D.cpp
@@ -46,28 +46,54 @@ void rot3D(int n, float rot_x, float rot_y, float rot_z, float *x, float *y, flo
 	rot_y *= PI;
 	rot_z *= PI;
 
+	auto cos_z = cos(rot_z);
+ 	auto sin_z = sin(rot_z);
+
   for (int i = 0; i < n; i++) {
+    float x_prev;
+    float y_prev;
+/*		
+    float z_prev;
+
 		// Rotation around x-axis
-    float y_prev = y[i];
-    float z_prev = z[i];
+    y_prev = y[i];
+    z_prev = z[i];
 
     y[i] = y_prev * cos(rot_x) - z_prev * sin(rot_x);
     z[i] = y_prev * sin(rot_x) + z_prev * cos(rot_x);
 
 		// Rotation around y-axis
-    float x_prev = x[i];
+    x_prev = x[i];
     z_prev = z[i];
 
     x[i] = x_prev *  cos(rot_y) + z_prev * sin(rot_y);
     z[i] = x_prev * -sin(rot_y) + z_prev * cos(rot_y);
-
+*/
 		// Rotation around z-axis
     x_prev = x[i];
     y_prev = y[i];
 
-    x[i] = x_prev * cos(rot_z) - y_prev * sin(rot_z);
-    y[i] = x_prev * sin(rot_z) + y_prev * cos(rot_z);
+    x[i] = x_prev * cos_z - y_prev * sin_z;
+    y[i] = x_prev * sin_z + y_prev * cos_z;
   }
+}
+
+
+// ============================================================================
+// Kernel Helpers
+// ============================================================================
+
+/**
+ * Subprogram to perform the rotations on a single item
+ *
+ * TODO make this fully 3D
+ */
+void rotate_item(Float &cos_z, Float &sin_z, Float::Ptr x, Float::Ptr y) {
+	// Rotation around z-axis
+  Float x_prev = *x;
+  Float y_prev = *y;
+  *x = x_prev * cos_z - y_prev * sin_z;
+  *y = y_prev * cos_z + x_prev * sin_z;
 }
 
 
@@ -77,10 +103,7 @@ void rot3D(int n, float rot_x, float rot_y, float rot_z, float *x, float *y, flo
 
 void rot3D_1(Int n, Float cosTheta, Float sinTheta, Float::Ptr x, Float::Ptr y) {
   For (Int i = 0, i < n, i += 16)
-    Float xOld = x[i];
-    Float yOld = y[i];
-    x[i] = xOld * cosTheta - yOld * sinTheta;
-    y[i] = yOld * cosTheta + xOld * sinTheta;
+		rotate_item(cosTheta, sinTheta, (x + i), (y + i));
   End
 }
 
@@ -104,8 +127,7 @@ void rot3D_1a(Int n, Float cosTheta, Float sinTheta, Float::Ptr x, Float::Ptr y)
     read(xOld, x);
     read(yOld, y);
 
-    *x = xOld * cosTheta - yOld * sinTheta;
-    *y = yOld * cosTheta + xOld * sinTheta;
+		rotate_item(cosTheta, sinTheta, x, y);
 
     x += step;
     y += step;
@@ -124,17 +146,22 @@ void rot3D_2(Int n, Float cosTheta, Float sinTheta, Float::Ptr x, Float::Ptr y) 
 
   gather(p); gather(q);
  
-  Float xOld, yOld;
+  Float x_prev, y_prev;
+
   For (Int i = 0, i < n, i += inc)
     gather(p+inc); gather(q+inc); 
-    receive(xOld); receive(yOld);
+    receive(x_prev); receive(y_prev);
 
-    *p = xOld * cosTheta - yOld * sinTheta;
-    *q = yOld * cosTheta + xOld * sinTheta;
+    *p = x_prev * cosTheta - y_prev * sinTheta;
+    *q = y_prev * cosTheta + x_prev * sinTheta;
+
+		// Can't do this directly
+		//rotate_item(cosTheta, sinTheta, p, q);
+
     p += inc; q += inc;
   End
 
-  receive(xOld); receive(yOld);
+  receive(x_prev); receive(y_prev);
 }
 
 
@@ -143,8 +170,8 @@ void rot3D_2(Int n, Float cosTheta, Float sinTheta, Float::Ptr x, Float::Ptr y) 
 // ============================================================================
 
 namespace {
-  int N = -1;       // Number of elements in incoming arrays for rot3D_3
-  int numQPUs = -1; // Number of QPUs to use for rot3D_3
+  int N       = -1;  // Number of elements in incoming arrays for rot3D_3
+  int numQPUs = -1;  // Number of QPUs to use for rot3D_3
 }  // anon namespace
 
 
@@ -165,13 +192,14 @@ void rot3D_3(Float cosTheta, Float sinTheta, Float::Ptr x, Float::Ptr y) {
 
   gather(p_src, q_src);
  
-  Float xOld, yOld;
-  For (Int i = 0, i < count, i++)
-    receive(xOld, p_src);
-    receive(yOld, q_src);
+  Float x_prev, y_prev;
 
-    *p_dst = xOld * cosTheta - yOld * sinTheta;
-    *q_dst = yOld * cosTheta + xOld * sinTheta;
+  For (Int i = 0, i < count, i++)
+    receive(x_prev, p_src);
+    receive(y_prev, q_src);
+
+    *p_dst = x_prev * cosTheta - y_prev * sinTheta;
+    *q_dst = y_prev * cosTheta + x_prev * sinTheta;
 
     p_dst.inc();
     q_dst.inc();
