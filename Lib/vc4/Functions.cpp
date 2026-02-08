fad5857ed397f04383728d73fc4e9de065fe55d9
Wim Rijnders <wrijnders@gmail.com>
2026-02-08 10:40:27 +0100

    Update commenting

--- Lib/vc4/Functions.cpp
+++ Lib/vc4/Functions.cpp
@@ -47,13 +47,27 @@ void hostIRQ() {
  * The implementation uses semaphores internally.
  * This is code for the Source level. It should be called within kernels.
  *
- * **NOT WORKING**. The final `barrier()` call on end of program is OK, within the 
- * program it blocks (Gravity example).  
- * Obviously, I don't understand it well enough.
  *
  * @param do_irq  if set, set a host interrupt
  *
  * ---------------------------------
+ *
+ * NOT WORKING
+ * -----------
+
+ * The final `barrier()` call on end of program is OK, within the 
+ * program it blocks (See Gravity example).
+ *
+ * The error on `vc4`:
+ *
+ *     ERROR: mbox_property() ioctl failed, ret: -1, error: Unknown error -1
+ *     ERROR: execute_qpu(): mbox_property call failed
+ *
+ * Making the host interrupt conditional didn't help.
+ * Obviously, I don't understand it well enough.
+ *
+ * NOTES
+ * -----
  *
  * Note that most semaphore stuff is under DMA.
  * Strictly speaking, this is not necessary, because semaphore are not explicitly linked to DMA.
