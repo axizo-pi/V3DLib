b9b2d2244582e6edb7ad8bf997f840c4921a1530
Wim Rijnders <wrijnders@gmail.com>
2026-03-30 13:44:17 +0200

    Added Examination of Gravity performance pi2

--- Examples/Gravity/main.cpp
+++ Examples/Gravity/main.cpp
@@ -19,7 +19,7 @@ void init_platform() {
   }
 
   // Disable TMU load - does nothing for vc7
-  //LibSettings::tmu_load tmu(false);
+  LibSettings::tmu_load tmu(false);
 }
 
 
@@ -38,24 +38,52 @@ void init_platform() {
  * Clearly something is different now since last check (too long ago).
  * Perhaps memory access, which I tinkered on.
  *
+ * I remember that adding barrier made a massive difference; now it doesn't do anything
+ * for _any_ pi.
+ *
+ * ## Pi2, vc4
+ *
+ * | barrier | QPU's | TMU | L2 Cache | Output OK | time | mbox error | Comment |
+ * | ------- | ----- | --- | -------- | --------- | ---- | ---------- | ------- |
+ * | yes     |  1    | yes | disabled | yes       | 42s  | no,  5     | Lost power at 6            |
+ * | yes     |  2    | yes | disabled | yes       | 34s  | no,  3     | |
+ * | yes     |  6    | yes | disabled | ?         | -    | YES, 1     | mbox error consistent at 1 |
+ * | yes     | 12    | yes | disabled | ?         | -    | YES, 1     | mbox error consistent at 1 |
+ * | no      |  1    | yes | disabled | yes       | 42s  | no,  3     | |
+ * | no      |  2    | yes | disabled | yes       | 32s  | no,  3     | |
+ * | no      |  3    | yes | disabled | yes       | 27s  | no,  3     | faster than n=12!?         |
+ * | no      |  6    | yes | disabled | yes       | 26s  | no,  3     | idem |
+ * | no      |  9    | yes | disabled | yes       | 27s  | no,  4     | idem |
+ * | no      | 12    | yes | disabled | yes       | 29s  | no,  3     | |
+ * | no      |  1    | no  | disabled | yes       | 41s  | no,  2     | |
+ * | no      |  6    | no  | disabled | yes       | 26s  | no,  2     | faster than n=12!?         |
+ * | no      | 12    | no  | disabled | yes       | 28s  | no,  3     | |
+ *
+ * ### Conclusions
+ *
+ * - The power socket of the pi2-1 appears to be sketchy; power lost during testing.
+ *   The connection is not firm.
+ * - barrier has no effect; it leads to mbox errors for n >= 6
+ * - Less QPU's works better!
+ *
  * ## Pi3, vc4
  *
- * | barrier | QPU's | TMU | L2 Cache | Ouput OK | time | mbox error | Comment |
- * | ------- | ----- | --- | -------- | -------- | ---- | ---------- | ------- |
- * | yes     |  1    | yes | enabled  | yes      | 56s  | no,  5     |         |
- * | "       | 12    | "   | "        | "        | 50s  | yes, 4     |         |
- * | "       |  1    | "   | disabled | "        | 36s  | no,  5     |         |
- * | "       | 12    | "   | disabled | "        | 35s  | no,  5     |         |
- * | "       |  1    | no  | disabled | NO       | 65s  | no,  1     |         |
- * | "       | 12    | no  | disabled | NO       | -    | yes, 1     | mbox error consistent at 1         |
- * | no      |  1    | "   | enabled  | yes      | 56s  | no,  5     |         |
- * | "       | 12    | "   | "        | "        | 34s  | no,  5     |         |
- * | "       |  1    | "   | disabled | "        | 36s  | no,  5     |         |
- * | "       | 12    | "   | disabled | "        | 19s  | no,  5     |         |
- * | "       |  1    | no  | enabled  | NO       | 83s  | no,  2     |         |
- * | "       | 12    | no  | enabled  | NO       | 62s  | no,  2     |         |
- * | "       |  1    | no  | disabled | NO       | 62s  | no,  2     |         |
- * | "       | 12    | no  | disabled | NO       | 65s  | no,  2     |         |
+ * | barrier | QPU's | TMU | L2 Cache | Output OK | time | mbox error | Comment |
+ * | ------- | ----- | --- | -------- | --------- | ---- | ---------- | ------- |
+ * | yes     |  1    | yes | enabled  | yes       | 56s  | no,  5     |         |
+ * | "       | 12    | "   | "        | "         | 50s  | yes, 4     |         |
+ * | "       |  1    | "   | disabled | "         | 36s  | no,  5     |         |
+ * | "       | 12    | "   | disabled | "         | 35s  | no,  5     |         |
+ * | "       |  1    | no  | disabled | NO        | 65s  | no,  1     |         |
+ * | "       | 12    | no  | disabled | NO        | -    | yes, 1     | mbox error consistent at 1 |
+ * | no      |  1    | "   | enabled  | yes       | 56s  | no,  5     |         |
+ * | "       | 12    | "   | "        | "         | 34s  | no,  5     |         |
+ * | "       |  1    | "   | disabled | "         | 36s  | no,  5     |         |
+ * | "       | 12    | "   | disabled | "         | 19s  | no,  5     |         |
+ * | "       |  1    | no  | enabled  | NO        | 83s  | no,  2     |         |
+ * | "       | 12    | no  | enabled  | NO        | 62s  | no,  2     |         |
+ * | "       |  1    | no  | disabled | NO        | 62s  | no,  2     |         |
+ * | "       | 12    | no  | disabled | NO        | 65s  | no,  2     |         |
  *
  * ### Conclusions
  *
@@ -64,12 +92,13 @@ void init_platform() {
  * - barrier has no effect; it can only decrease performance
  *
  * ## Pi4, vc6
- * | barrier | QPU's | TMU | L2 Cache | Ouput OK | time | mbox error | Comment |
- * | ------- | ----- | --- | -------- | -------- | ---- | ---------- | ------- |
- * | yes     |  1    | -   | -        | yes      | 20s  | no,  3     |         |
- * | yes     | 16    | -   | -        | yes      | 13s  | no,  3     |         |
- * | no      |  1    | -   | -        | yes      | 20s  | no,  3     |         |
- * | no      |  8    | -   | -        | yes      | 13s  | no,  3     |         |
+ *
+ * | barrier | QPU's | TMU | L2 Cache | Ouitput OK | time | mbox error | Comment |
+ * | ------- | ----- | --- | -------- | --------- | ---- | ---------- | ------- |
+ * | yes     |  1    | -   | -        | yes       | 20s  | no,  3     |         |
+ * | yes     | 16    | -   | -        | yes       | 13s  | no,  3     |         |
+ * | no      |  1    | -   | -        | yes       | 20s  | no,  3     |         |
+ * | no      |  8    | -   | -        | yes       | 13s  | no,  3     |         |
  *
  * ### Conclusions
  *
@@ -87,7 +116,6 @@ void init_platform() {
  * ### Conclusions
  *
  * - `barrier` has no effect (WTF)
- * 
  */
 int main(int argc, const char *argv[]) {
   settings.init(argc, argv);
