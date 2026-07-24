34c003877c439c1705fc49fcc953799e38006b3f
Wim Rijnders <wrijnders@gmail.com>
2026-07-24 05:33:35 +0200

    uint8 <-> int (both ways) working

--- Tests/testConversions.cpp
+++ Tests/testConversions.cpp
@@ -1,10 +1,52 @@
 #include <V3DLib.h>
 #include "support/support.h"
 #include "support/check.h"
+#include "Support/Helpers.h"  // to_file()
 
 using namespace V3DLib;
 
 namespace {
+
+
+///////////////////////////////////////////////////
+// Helpers
+///////////////////////////////////////////////////
+
+std::string dump_uint8_val(int val) {
+  std::string ret;
+
+  // Bitwise conversion to avoid issues with signs
+  unsigned tmp = *((uint32_t *) &val);
+
+  ret << (tmp & 0xff)         << ", "
+      << ((tmp >>  8) & 0xff) << ", "
+      << ((tmp >> 16) & 0xff) << ", "
+      << ((tmp >> 24) & 0xff)
+	;
+
+  return ret;
+};
+
+
+/**
+ * @brief Display uint8 values
+*/
+MAYBE_UNUSED std::string dump_uint8(Int::Array const &values) {
+  std::string ret;
+	//ret << "size: " << values.size() << " <";
+
+  for (int i = 0; i < (int) values.size(); i++) {
+    ret << dump_uint8_val(values[i]) << ", ";
+  }
+
+	//ret << ">";
+	return ret;
+}	
+
+
+///////////////////////////////////////////////////
+// Kernels
+///////////////////////////////////////////////////
 
 void signed_to_float_kernel(Float::Ptr result, Int::Ptr input, Int Blocks) {
   For (Int i = 0, i < Blocks, i++)
@@ -121,6 +163,7 @@ void duplicate_first_4_elements_partial(Int &res, Int &val) {
   End
 }
 
+
 /**
  * @brief Convert incoming uint8 values to Int.
  *
@@ -131,47 +174,126 @@ void uint8_to_Int_partial(Int &res, Int &val) {
   duplicate_first_4_elements_partial(res, val);
 
   Where (index() % 4 == 0)
-    res = (res >> 24) & 0xff;
-  End
-
-  Where (index() % 4 == 1)
-    res = (res >> 16) & 0xff;
-  End
-
-  Where (index() % 4 == 2)
-    res = (res >> 8) & 0xff;
-  End
-
-  Where (index() % 4 == 3)
     res = res & 0xff;
   End
 
+  Where (index() % 4 == 1)
+    res = (res >> 8) & 0xff;
+  End
+
+  Where (index() % 4 == 2)
+    res = (res >> 16) & 0xff;
+  End
+
+  Where (index() % 4 == 3)
+    res = (res >> 24) & 0xff;
+  End
+}
+
+
+/**
+ * @brief Convert incoming Int values to uint8.
+ *
+ * The result is 4 words long. 
+ * Overflow and sign change are negated and therefore ignored.
+ *
+ * TODO: incorporate this into unit test.
+ */
+void Int_to_uint8_partial(Int &res, Int &val) {
+  Int tmp = val;
+  tmp = tmp & 0xff;   // Mask to avoid issues with overflow and sign
+
+	Int tmp1;
+	Int tmp2;
+	Int tmp3;
+	tmp1 = rotate(tmp, 15);
+	tmp2 = rotate(tmp, 14);
+	tmp3 = rotate(tmp, 13);
+
+	// Move bytes to correct position
+	// tmp as is
+  tmp1 = (tmp1 <<  8);
+  tmp2 = (tmp2 << 16);
+  tmp3 = (tmp3 << 24);
+
+	tmp = tmp | tmp1 | tmp2 | tmp3;
+
+	//
+	// At this point, vector elements 0,4,8,12 are correct. The rest is junk.
+	// Move these to first four elements.
+	//
+	tmp1 = tmp;
+	tmp1 = rotate(tmp1, 13);
+	Where (index() == 1)
+		tmp = tmp1;
+	End
+
+	tmp1 = rotate(tmp1, 13);
+	Where (index() == 2)
+		tmp = tmp1;
+	End
+
+	tmp1 = rotate(tmp1, 13);
+	Where (index() == 3)
+		tmp = tmp1;
+	End
+
+/*		
+	// Zap the trailing elements - doesn't do much
+	Where (index() > 3)
+		tmp = 0;
+	End
+*/
+	res = tmp;
 }
 
 
 /**
  * @brief Kernel to test conversion uint8 <-> Int (both ways)
  *
+ * Lesson learnt and to remember: ARM endianess is LSB.
+ *
  * @param res_int Array to store internal Int values, to check them
+ * @param values  Incoming values; every byte is a uint8 value
  * @param N       Number of incoming 4-byte values
  */
-void uint8_to_Int_kernel(Int::Ptr res_int, Int::Ptr values, Int N) {
+void uint8_to_Int_kernel(Int::Ptr res_int, Int::Ptr res, Int::Ptr values, Int N) {
+/*	
+	res -= index();
+	Where (index() < 4)
+		res += index();
+	Else
+		res += 4;
+	End
+*/	
+/*	
+	Where (index() >4)
+		res -= index();
+		res += 4;
+	End
+*/	
+
   For (Int n = 0, n < N, n += 4)
     Int val = *values;
-    Int res = 0;
+    Int internal = 0;
 
-    uint8_to_Int_partial(res, val);
+    uint8_to_Int_partial(internal, val);
 
     // At this point, res contains the next 16 uint8 values
 
-    *res_int = res;
+    *res_int = internal;  // Store internal Int values, for testing
 
-    //
-    // TODO: Some kind of operation here
-    //
+    // Do some kind of operation here on internal
+		internal *= 2;
 
-    values += 4;
-    res_int.inc();
+		// Store results as uint8
+		Int result;
+		Int_to_uint8_partial(result, internal);
+		*res = result;
+
+    values += 4;   comment("Do pointer increments");
+    res    += 4;
+    res_int.inc(); comment("Increment res_int");
   End
 }  
 
@@ -284,7 +406,7 @@ TEST_CASE("Test conversion uint8 -> float [convert]") {
     unsigned val1 = values[n + 1];
     unsigned val2 = values[n + 2];
     unsigned val3 = values[n + 3];
-    unsigned tmp = (val0 << 24) + (val1 << 16) + (val2 << 8) + val3;
+    unsigned tmp = (val3 << 24) + (val2 << 16) + (val1 << 8) + val0;
 
     // Bitwise conversion to avoid issues with signs
     ret  = *((int *) & tmp);
@@ -293,31 +415,17 @@ TEST_CASE("Test conversion uint8 -> float [convert]") {
     return ret;
   };
 
-
-  auto dump_uint8 = [] (int val) -> std::string {
-    std::string ret;
-
-    // Bitwise conversion to avoid issues with signs
-    unsigned tmp = *((uint32_t *) &val);
-
-    ret << (tmp >> 24)          << ", "
-        << ((tmp >> 16) & 0xff) << ", "
-        << ((tmp >>  8) & 0xff) << ", "
-        << (tmp & 0xff);
-
-    return ret;
-  };
-
-
   std::vector<uint8_t> values_8 = {
     0, 1, 2, 3,
     4, 5, 6, 7,
     8, 9, 10, 11,
     12, 13, 14, 15,
 
+		// Test limits for overflow
+    0,   127,   1,  64,
+    0,   128,   1, 255,
+
     // Arbitrary values
-    0,   124,   1,   0,
-    1,     1, 124, 124,
     2,   100,   4, 100,
     6,   100,   8, 100,
     16,  100,  32, 100,
@@ -339,22 +447,36 @@ TEST_CASE("Test conversion uint8 -> float [convert]") {
     values[i] = uint8_to_int(values_8, 4*i);
   }
 
-  // Display uint8 values
-  std::string buf;
-  for (int i = 0; i < 16; i++) {
-    buf << dump_uint8(values[i]) << ", ";
-  }
-  warn << "values: <" << buf << ">";
+  //warn << "values         :  " << dump_uint8(values);
 
-  Int::Array result(4*16);
-  result.fill(-1);
+  //
+  // Perform the actual test
+  //
+  Int::Array result_internal(4*16);
+  result_internal.fill(-1);
+
+  Int::Array result(16 + 1);
+  result.fill(-2);
 
   auto k = compile(uint8_to_Int_kernel);
-  k.load(&result, &values, (int) values.size()).run();
+	//to_file("uint8_to_Int_kernel.txt", k.dump());
+  k.load(&result_internal, &result, &values, (int) values.size()).run();
 
-  warn << "result: " << dump_array(result);
+  //warn << "result_internal: " << dump_array(result_internal);
+  //warn << "result         : " << dump_uint8(result);
+
+	// Check internal values
+  for (int i = 0; i < (int) values_8.size(); ++i) {
+		INFO("Check internal i = " << i);
+    REQUIRE(values_8[i] == result_internal[i]);
+  }
+
+	// Check result
+	uint8_t *result_ptr = (uint8_t *) result.ptr();
 
   for (int i = 0; i < (int) values_8.size(); ++i) {
-    REQUIRE(values_8[i] == result[i]);
-  }
+		INFO("Check result i = " << i);
+		uint8_t val = 2* values_8[i];   // too big values will overflow
+    REQUIRE(val == result_ptr[i]);
+	}
 }
